f50f51f4c32506fef964617cecdafe434be28673
vlj2007 <vlj2007@yandex.ru>
2024-01-02 01:23:53 +0700

    I created three classes

--- src/main/java/ru/hogwarts/school/controller/FacultyController.java
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -1,8 +1,12 @@
 package ru.hogwarts.school.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.service.FacultyService;
+
 @RestController
+@RequestMapping("faculties")
 public class FacultyController {
 
     private final FacultyService facultyService;
@@ -10,4 +14,16 @@ public class FacultyController {
     public FacultyController(FacultyService facultyService) {
         this.facultyService = facultyService;
     }
+
+    @PostMapping //POST localhost:8080/faculty/1
+    public Faculty createFaculty(Faculty faculty) {
+        return facultyService.createdFaculty(faculty);
+    }
+
+    @GetMapping("{id}") //GET localhost:8080/faculty/1
+    public Faculty getFacultyInfo(@PathVariable long id) {
+        return facultyService.findFaculty(id);
+    }
+
+
 }
